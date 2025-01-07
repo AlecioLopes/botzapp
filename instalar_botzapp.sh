309d36700e65e156c952410475ae519279e9c91d
Alécio Lopes <121270552+AlecioLopes@users.noreply.github.com>
2025-01-07 15:58:59 -0300

    Update instalar_botzapp.sh

--- instalar_botzapp.sh
+++ instalar_botzapp.sh
@@ -9,14 +9,30 @@ echo "Git e Go foram instalados corretamente."
 # Clonar o repositório do BotzApp
 echo "Clonando o repositório do BotzApp..."
 git clone https://github.com/AlecioLopes/botzapp.git &>/dev/null
+
+# Verificar se o diretório botzapp foi criado
+if [ ! -d "botzapp" ]; then
+    echo "Erro: O diretório 'botzapp' não foi criado. Verifique o clone do repositório."
+    exit 1
+fi
 echo "Repositório clonado com sucesso."
 
 # Navegar para o diretório do projeto
-cd botzapp
+echo "Tentando acessar o diretório 'botzapp'..."
+cd botzapp || { echo "Erro: Não foi possível acessar o diretório 'botzapp'"; exit 1; }
+
+# Verificar se estamos no diretório correto
+if [ "$(pwd)" != "$(realpath botzapp)" ]; then
+    echo "Erro: O diretório 'botzapp' não é o diretório esperado."
+    exit 1
+fi
 
 # Compilar o binário do BotzApp com o nome padrão
 echo "Compilando o binário..."
-go build . &>/dev/null
+if ! go build . &>/dev/null; then
+    echo "Erro ao compilar o binário do BotzApp."
+    exit 1
+fi
 
 # Verificar se o binário foi compilado corretamente
 if [ -f "./wuzapi" ]; then
@@ -24,7 +40,7 @@ if [ -f "./wuzapi" ]; then
     
     # Dar permissões de execução ao binário
     chmod +x wuzapi
-    chmod +x executar_wuzapi.sh
+    chmod +x executar_botzapp.sh
 
     echo "Permissões de execução concedidas ao BotzApp."
 else
